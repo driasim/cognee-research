7e0a8aac8f75e4ba72194bed96e3e5a017bec0a1
driasim <driasim@users.noreply.github.com>
2026-06-04 15:33:06 +0000

    [spark-compete] enhance(cli): add --no-color flag to search command

--- cognee/cli/commands/search_command.py
+++ cognee/cli/commands/search_command.py
@@ -76,6 +76,7 @@ Search Types & Use Cases:
             choices=OUTPUT_FORMAT_CHOICES,
             default="pretty",
             help="Output format (default: pretty)",
+        parser.add_argument("--no-color", action="store_true", help="Disable colored output")
         )
 
     def execute(self, args: argparse.Namespace) -> None:

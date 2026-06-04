53c957744fe7fefa7108245d49f2cca3ee23602b
driasim <driasim@users.noreply.github.com>
2026-06-04 15:32:49 +0000

    [spark-compete] enhance(cli): add --max-results flag to search command

--- cognee/cli/commands/search_command.py
+++ cognee/cli/commands/search_command.py
@@ -76,6 +76,7 @@ Search Types & Use Cases:
             choices=OUTPUT_FORMAT_CHOICES,
             default="pretty",
             help="Output format (default: pretty)",
+        parser.add_argument("--max-results", type=int, help="Maximum number of results to return")
         )
 
     def execute(self, args: argparse.Namespace) -> None:
